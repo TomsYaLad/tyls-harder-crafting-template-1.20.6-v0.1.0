5498c5d90c4992cdb11ed0941c2c3bcd919311a5
TomsYaLad <tomanr234@gmail.com>
2024-06-18 19:37:15 +1000

    added to github

---
+++ src/main/java/net/tomsyalad/hardercrafting/TYLsHarderCraftingClient.java
@@ -0,0 +1,10 @@
+package net.tomsyalad.hardercrafting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TYLsHarderCraftingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tomsyalad/hardercrafting/TYLsHarderCraftingDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tomsyalad.hardercrafting;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TYLsHarderCraftingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
